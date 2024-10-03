cee84d483c5f7981927242f2d234980444d01df4
Krishan Maan <krishankumarmaan400@gmail.com>
2024-10-03 21:56:37 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+
+
+$(function(){
+  $("#deskHeader").load("includes/headerPage.html"); 
+  $("#mFooter").load("includes/footerPage.html"); 
+  $("#zeroint").load("includes/zeroPage.html"); 
+});
+
+   /*----------------------------------------------------*/
+    /*  Clients Slider
+    /*----------------------------------------------------*/
+    function testi_slider(){
+        if ( $('.testi_slider').length ){
+            $('.testi_slider').owlCarousel({
+                loop:true,
+                margin: 0,
+                items: 1,
+                nav:false,
+                autoplay: true,
+                smartSpeed: 1500,
+                dots:true,
+            })
+        }
+    }
+    testi_slider();
+//BackToTop
+function BackToTop() {
+    $('.back-to-top').on('click', function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 100);
+      return false;
+    });
+    $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      if (y > 600) {
+        $('.back-to-top').fadeIn();
+        $('.back-to-top').addClass('open');
+      } else {
+        $('.back-to-top').fadeOut();
+        $('.back-to-top').removeClass('open');
+      }
+    });
+  }
+  BackToTop();
+  //Animation
+AOS.init({
+  duration: 1200 });
+//# sourceURL=pen.js
+ 
